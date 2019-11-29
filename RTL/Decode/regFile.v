b8f457421f85b73f42dd05a466d24f0872d5d0b5
Marc Igarza <marc.igarza@bsc.es>
2019-11-29 09:12:11 +0100

    Rearrange of folders

--- RTL/Decode/regFile.v
+++ RTL/Decode/regFile.v
@@ -1,19 +1,27 @@
-module regFile (clk, rst, src1, src2, dest, writeVal, writeEn, reg1, reg2);
+module regFile (clk, rst, src1, src2, dest, writeVal, writeEn, reg1, reg2, rmPC; rmAddr, excV);
   input clk, rst, writeEn;
   input [4:0] src1, src2, dest;
   input [31:0] writeVal;
   output [31:0] reg1, reg2;
 
   reg [31:0] regMem [4:0];
+  reg [31:0] rm0;
+  reg [31:0] rm1;
+
   integer i;
 
   always @ (posedge clk) begin
     if (rst) begin
+      rm0 <= 0;
+      rm1 <= 0;
       for (i = 0; i < 31; i = i + 1)
         regMem[i] <= 0;
-	    end
+    end
 
     else if (writeEn) regMem[dest] <= writeVal;
+    else if (excV) 
+	    rm0 <= rmPC;
+    	    rm1 <= rmAddr;
   end
 
   assign reg1 = (regMem[src1]);
