0a91d716c8eeb318c6544944514305ef951d68ba
Marc Igarza <marc.igarza@bsc.es>
2019-12-11 11:48:37 +0100

    Added new ByPass from MEM to ALU. Need check

--- RTL/alu/alu_top.v
+++ RTL/alu/alu_top.v
@@ -16,20 +16,31 @@ module alu_top
   output logic [`PC_WIDTH-1:0]	 		pcBranch,
 
   output logic 			 		pcBranchVal,
-
-  output logic					writeEn
+ 
+  output logic					writeEn,
+  
+  input  logic [`REG_FILE_RANGE]  		rdIn, //We are lucky that the address and the register are the same size
+  output logic [`REG_FILE_RANGE]  		rdOut,
+  
+  //ByPasses
+  input logic [`REG_FILE_RANGE]  		memOutBP,
 );
 
+logic [`INSTR_OPCODE-1:0]              prev_instr;
 
-logic [`REG_FILE_RANGE]  aluOut_next;
-logic [`PC_WIDTH-1:0]    pcBranch_next;
-logic			 pcBranchVal_next;
-logic			 writeEn_next;
+logic [`REG_FILE_RANGE]  	       aluOut_next;
+logic [`PC_WIDTH-1:0]    	       pcBranch_next;
+logic			 	       pcBranchVal_next;
+logic			               writeEn_next;
+
+
+`RST_FF(clock, reset_c, prev_instr, instr,   '0)
 
 `RST_FF(clock, reset_c, aluOut, aluOut_next, '0)
 `RST_FF(clock, reset_c, pcBranch, pcBranch_next, '0)
 `RST_FF(clock, reset_c, pcBranchVal, pcBranchVal_next, 1'b0)
-`RST_FF(clock, reset_c, , writeEn, writeEn_next, 1'b0)
+`RST_FF(clock, reset_c, writeEn, writeEn_next, 1'b0)
+
 
 always_comb
 begin
@@ -37,24 +48,25 @@ begin
 	aluOut_next  = '0;
 	pcBranchVal  = '0;
 	writeEn_next = '0;
-
 	if (instr == 8'h00)
 	begin
-		aluOut_next <= val1 + val2;
+		aluOut_next = val1 + val2;
 		writeEn_next = 0'b1;
 	end
 	if (instr == 8'h01)
         begin
-                aluOut_next <= val1 - val2;
+                aluOut_next = val1 - val2;
                 writeEn_next = 0'b1;
         end
 	if (instr == 8'h02)
         begin
-                aluOut_next <= val1 * val2;
+                aluOut_next = val1 * val2;
                 writeEn_next = 0'b1;
         end
 	if (instr == 8'h1X) //MEM
-                aluOut_next <= val1 + val2;
+                aluOut_next = val1 + val2;
+		if (instr == 8'h10 || instr == 8'h11)
+			rdOut = ((instr == 8'h12 || instr == 8'h13) && aluOut == aluOut_next)? memOutBP : rdIn;//Does this logic work in verilog?
 	if (instr == 8'h31) //JUMP
 	begin
 		pcBranch_next = val1;
@@ -72,6 +84,5 @@ begin
         begin
         //TODO
         end
-
 end
 endmodule
