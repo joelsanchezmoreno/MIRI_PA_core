cbcdee9b3d61ab2aee5adec69a952e0a90b0ca65
joel.sanchez <joel.sanchez@esperantotech.com>
2019-12-19 15:35:38 +0100

    [Fetch] Icache LRU has been deleted to use a parameterizable module + adding verbosity for debugging + solving compilation errors

--- rtl/fetch/fetch_top.v
+++ rtl/fetch/fetch_top.v
@@ -33,6 +33,14 @@ module fetch_top
  );
 assign xcpt_fetch = '0; //FIXME: connect to iTLB
 
+
+// Response from the Instruction Cache
+logic                                   icache_rsp_valid;
+logic [`ICACHE_LINE_WIDTH-1:0]          icache_rsp_data;
+logic [`ICACHE_INSTR_IN_LINE_WIDTH-1:0] word_in_line;
+logic [`INSTR_WIDTH-1:0]                decode_instr_data_next;
+
+// Program counter
 logic   [`PC_WIDTH-1:0] program_counter;
 logic   [`PC_WIDTH-1:0] program_counter_next;
 logic                   program_counter_update;
@@ -40,22 +48,27 @@ logic                   program_counter_update;
 //         CLK    RST    EN                      DOUT             DIN                   DEF
 `RST_EN_FF(clock, reset, program_counter_update, program_counter, program_counter_next, boot_addr)
 
-assign program_counter_update   = ( stall_fetch | !icache_ready) ? 1'b0 : 1'b1;
+assign program_counter_update   = ( stall_fetch | !icache_ready | !icache_rsp_valid) ? 1'b0 : 1'b1;
 assign program_counter_next     = ( take_branch ) ? branch_pc : 
                                                     program_counter + 4;
 
-
+`ifdef VERBOSE_FETCH
+always_ff @(posedge clock)
+begin
+    if (program_counter_update)
+    begin
+        $display("[FETCH] Program counter value is %h",program_counter);
+        $display("[FETCH] Program counter next value is %h",program_counter_next);
+    end
+end
+`endif
+ 
 // Request to the Instruction Cache
 logic icache_req_valid;
 logic icache_ready;
 
 assign icache_req_valid = !stall_fetch & icache_ready;
 
-// Response from the Instruction Cache
-logic                                   icache_rsp_valid;
-logic [`ICACHE_LINE_WIDTH-1:0]          icache_rsp_data;
-logic [`ICACHE_INSTR_IN_LINE_WIDTH-1:0] word_in_line;
-logic [`INSTR_WIDTH-1:0]                decode_instr_data_next;
 
 //     CLK    EN            DOUT                DIN                   
 `EN_FF(clock, !stall_fetch, decode_instr_data,  decode_instr_data_next)
@@ -67,16 +80,6 @@ always_comb
 begin
     word_in_line            = program_counter[`ICACHE_INSTR_IN_LINE];
     decode_instr_data_next  = icache_rsp_data[`INSTR_WIDTH*word_in_line+:`INSTR_WIDTH];
-end
-
-// Request to the memory hierarchy
-logic [`ICACHE_ADDR_WIDTH-1:0]   req_addr_miss;
-
-always_comb
-begin
-    req_info_miss.addr      = req_addr_miss;
-    req_info_miss.is_store  = 1'b0;
-    req_info_miss.data      = '0;
 end
 
 instruction_cache
@@ -95,7 +98,7 @@ icache(
     .rsp_data           ( icache_rsp_data   ),
     
     // Request to the memory hierarchy
-    .req_addr_miss      ( req_addr_miss     ),
+    .req_info_miss      ( req_info_miss     ),
     .req_valid_miss     ( req_valid_miss    ),
                     
     // Response from the memory hierarchy
--- rtl/fetch/instruction_cache.v
+++ rtl/fetch/instruction_cache.v
@@ -3,6 +3,7 @@
 // ensure that one request can be served each cycle if there are no misses.
 // Otheriwse, in case of a miss it takes `MAIN_MEMORY_LATENCY cycles to go to 
 // memory and bring the line. 
+`include "soc.vh"
 
 module instruction_cache
 (
@@ -29,9 +30,12 @@ module instruction_cache
 
 //////////////////////////////////////////////////
 // Instruction Cache arrays: tag, data and valid
-logic [`ICACHE_LINE_WIDTH-1:0]  instMem_data,instMem_data_ff [`ICACHE_NUM_WAYS-1:0];
-logic [`ICACHE_TAG_RANGE]       instMem_tag, instMem_tag_ff  [`ICACHE_NUM_WAYS-1:0];
-logic [`ICACHE_NUM_WAYS-1:0]    instMem_valid, instMem_valid_ff;
+logic [`ICACHE_LINE_WIDTH-1:0] instMem_data     [`ICACHE_NUM_WAYS-1:0];
+logic [`ICACHE_LINE_WIDTH-1:0] instMem_data_ff  [`ICACHE_NUM_WAYS-1:0];
+logic [`ICACHE_TAG_RANGE]      instMem_tag      [`ICACHE_NUM_WAYS-1:0];
+logic [`ICACHE_TAG_RANGE]      instMem_tag_ff   [`ICACHE_NUM_WAYS-1:0];
+logic [`ICACHE_NUM_WAYS-1:0]   instMem_valid;
+logic [`ICACHE_NUM_WAYS-1:0]   instMem_valid_ff;
 
 //  CLK        DOUT         DIN         
 `FF(clock, instMem_data_ff, instMem_data)
@@ -42,31 +46,40 @@ logic [`ICACHE_NUM_WAYS-1:0]    instMem_valid, instMem_valid_ff;
 
 //////////////////////////////////////////////////
 // Control signals 
-logic tag_miss; // asserted when there is a miss on the instr. cache
 logic icache_hit;
-logic [`ICACHE_NUM_WAY_RANGE]       hit_way; 
+logic [`ICACHE_WAYS_PER_SET_RANGE]  hit_way; 
 logic [`ICACHE_TAG_RANGE]           req_addr_tag;
 logic [`ICACHE_NUM_WAY_RANGE]       req_addr_pos; // Position of the data in case there is a hit on tag array
+logic [`ICACHE_NUM_WAY_RANGE]       miss_icache_pos;
 
 //////////////////////////////////////////////////
 // Position of the victim to be evicted from the I$
-logic [`ICACHE_NUM_SET_RANGE]       req_addr_set,miss_icache_set_ff;  
-logic [`ICACHE_NUM_WAY_RANGE]       miss_icache_way, miss_icache_way_ff; 
+logic [`ICACHE_NUM_SET_RANGE]       req_addr_set;  
+logic [`ICACHE_NUM_SET_RANGE]       miss_icache_set_ff;  
+logic [`ICACHE_WAYS_PER_SET_RANGE]  miss_icache_way; 
+logic [`ICACHE_WAYS_PER_SET_RANGE]  miss_icache_way_ff; 
 
 //         CLK    RST    EN        DOUT                DIN              DEF
-`RST_EN_FF(clock, reset, tag_miss, miss_icache_set_ff, req_addr_set,    '0)
-`RST_EN_FF(clock, reset, tag_miss, miss_icache_way_ff, miss_icache_way, '0)
+`RST_EN_FF(clock, reset, !icache_hit, miss_icache_set_ff, req_addr_set,    '0)
+`RST_EN_FF(clock, reset, !icache_hit, miss_icache_way_ff, miss_icache_way, '0)
 
 //////////////////////////////////////////////////
 // Ready signal to stall the pipeline if ICache is busy
 logic icache_ready_next;
 
-//      CLK    RST    DOUT          DIN                DEF
-`RST_FF(clock, reset, icache_ready, icache_ready_next, '0)
+//      CLK    DOUT          DIN               
+//`FF(clock, icache_ready, icache_ready_next)
 
+//      CLK    RST    DOUT            DIN              DEF
+`RST_FF(clock, reset, icache_ready, icache_ready_next, 1'b1)
+
+logic pendent_req,pendent_req_ff;
+
+//      CLK    RST    DOUT            DIN           DEF
+`RST_FF(clock, reset, pendent_req_ff, pendent_req, 1'b0)
 
 integer iter;
-
+//logic [`ICACHE_WAYS_PER_SET_RANGE] iter;
 always_comb
 begin
     // Mantain values for next clock
@@ -74,38 +87,59 @@ begin
     instMem_tag         = instMem_tag_ff;
     instMem_data        = instMem_data_ff;
     icache_ready_next   = icache_ready;
+    pendent_req         = pendent_req_ff;
 
     // There is a miss if the tag is not stored
     req_addr_tag    = req_addr[`ICACHE_TAG_ADDR_RANGE];
     req_addr_set    = req_addr[`ICACHE_SET_ADDR_RANGE]; 
-    
-    icache_hit      = 1'b0;
     hit_way         = '0;
     req_addr_pos    = '0; 
+  
+    // Do not send request to MM until we ensure we miss on the icache
+    req_valid_miss      = 1'b0;
+    req_info_miss.data  = '0;
+    icache_hit          = 1'b0;
 
-    // Look if the tag is on the cache
-    for (iter = 0; iter < `ICACHE_WAYS_PER_SET; i++)
+    // Do not respond to the fetch top until we ensure we have the correct
+    // data
+    rsp_data        = '0;
+    rsp_valid       = 1'b0;
+
+    // If there is a request and we are not performing one
+    if (req_valid & !pendent_req_ff)
     begin
-        if ((instMem_tag[iter + req_addr_set*`ICACHE_WAYS_PER_SET]   == req_addr_tag) &
-             instMem_valid[iter + req_addr_set*`ICACHE_WAYS_PER_SET] == 1'b1)
+        // Look if the tag is on the cache
+        for (iter = 0; iter < `ICACHE_WAYS_PER_SET; iter++)
         begin
-            req_addr_pos      = iter + req_addr_set*`ICACHE_WAYS_PER_SET;
-            icache_hit        = 1'b1;
-            hit_way           = iter;
-        end
-    end
-    
-    // If there is a request from the fetch stage and there is no hit, we
-    // have a miss
-    tag_miss = (req_valid & icache_hit) ? 1'b0 : 1'b1;
+            if ((instMem_tag_ff[iter + req_addr_set*`ICACHE_WAYS_PER_SET]   == req_addr_tag) &
+                 instMem_valid_ff[iter + req_addr_set*`ICACHE_WAYS_PER_SET] == 1'b1)
+            begin
+                req_addr_pos      = iter + req_addr_set*`ICACHE_WAYS_PER_SET;
+                icache_hit        = 1'b1;
+                hit_way           = iter;
 
-    // If there is a miss we send a request to main memory to get the line
-    if ( tag_miss )
-    begin
-        req_info_miss.addr              = req_addr;
-        req_info_miss.is_store          = 1'b0;
-        req_valid_miss                  = 1'b1;
-        icache_ready_next               = 1'b0;
+                // Return data to fetch top
+                rsp_data    = instMem_data_ff[req_addr_pos];
+                rsp_valid   = 1'b1;
+
+                `ifdef VERBOSE_ICACHE
+                    $display("[ICACHE] TAG hit. Position in I$ is %h , way %h",req_addr_pos,hit_way);
+                `endif
+            end
+        end
+
+        // If there is a miss we send a request to main memory to get the line
+        if ( !icache_hit )
+        begin
+            `ifdef VERBOSE_ICACHE
+                $display("[ICACHE] TAG miss asserted. Requested addr is %h",req_addr);
+            `endif  
+            pendent_req                     = 1'b1;
+            req_info_miss.addr              = req_addr;
+            req_info_miss.is_store          = 1'b0;
+            req_valid_miss                  = !reset;
+            icache_ready_next               = 1'b0;
+        end
     end
 
     // We wait until we receive the response from main memory. Then, we update
@@ -114,24 +148,23 @@ begin
     if (rsp_valid_miss)
     begin
         miss_icache_pos = miss_icache_way_ff + miss_icache_set_ff*`ICACHE_WAYS_PER_SET;
+        `ifdef VERBOSE_ICACHE
+            $display("[ICACHE] Response from MM valid. instMem_tag[%h] = %h",miss_icache_pos,rsp_data_miss);
+        `endif            
         instMem_tag[miss_icache_pos]   = req_addr_tag;
         instMem_data[miss_icache_pos]  = rsp_data_miss;
         instMem_valid[miss_icache_pos] = 1'b1; 
+        pendent_req                    = 1'b0;
         icache_ready_next              = 1'b1;
+
+        // Return data to fetch top
+        rsp_valid   = 1'b1; 
+        rsp_data    = instMem_data[miss_icache_pos];
     end
 end
 
-assign rsp_data  = ( rsp_valid_miss ) ? instMem_data[miss_icache_pos] : // if there is a response for a miss
-                   ( !tag_miss      ) ? instMem_data_ff[req_addr_pos]    : // if we hit on the first access
-                                        '0;                             // default
-
-assign rsp_valid = ( rsp_valid_miss ) ? 1'b1 : // if there is a response for a miss
-                   ( !tag_miss      ) ? 1'b1 : // if we hit on the first access
-                                        1'b0;  // default
-
-
 logic [`ICACHE_NUM_SET_RANGE] update_set;  
-logic [`ICACHE_NUM_WAY_RANGE] update_way;  
+logic [`ICACHE_WAYS_PER_SET_RANGE] update_way;  
 
 assign update_set = (rsp_valid_miss) ? miss_icache_set_ff :
                     (icache_hit)     ? req_addr_set       :
@@ -142,7 +175,12 @@ assign update_way = (rsp_valid_miss) ? miss_icache_way_ff :
                     '0;              
 // This module returns the oldest way accessed for a given set and updates the
 // the LRU logic when there's a hit on the I$ or we bring a new line                        
-icache_lru
+cache_lru
+#(
+    .NUM_SET       ( `ICACHE_NUM_SET        ),
+    .NUM_WAYS      ( `ICACHE_NUM_WAYS       ),
+    .WAYS_PER_SET  ( `ICACHE_WAYS_PER_SET   )
+)
 icache_lru
 (
     // System signals
