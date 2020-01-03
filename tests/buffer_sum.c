8af723ef141f0db3fe26f451aea19224becd9025
Marc Igarza <marc.igarza@bsc.es>
2020-01-03 14:18:23 +0100

    Added buffer sum test description

--- tests/buffer_sum.c
+++ tests/buffer_sum.c
@@ -1,6 +1,9 @@
+#include <iostream>
+using namespace std;
 int main() {
 	int a[128], sum = 0;
 	int i;
 	for(i = 0; i < 128; i++) a[i] = i;
 	for(i = 0; i < 128; i++) sum += a[i];
+	cout << sum;
 }
